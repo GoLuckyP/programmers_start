e8d12fd6e11b51d7e334389009a115eb77a03c77
GoLuckyP <111550984+GoLuckyP@users.noreply.github.com>
2023-09-20 20:47:24 +0900

    [level 0] Title: 세균 증식, Time: 0.04 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int n, int t) {
         //n은 1시간에 n*2가 된다
         // n = n * 2의 t제곱
-        int answer = n * (int)Math.pow(2, t);
+        int answer = n * (int)Math.pow(2, t); //2점 이상 나오신 분...알려주세요
         return answer;
     }
 }
