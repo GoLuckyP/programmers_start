d8212568a2af200721a33a2e63685f193f86751f
GoLuckyP <111550984+GoLuckyP@users.noreply.github.com>
2023-10-02 21:14:43 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,18 +1,24 @@
+/*외계행성의 나이*/
+
 import java.util.Arrays;
 class Solution {
     public String solution(int age) {
-        
-        char[] ageChar = Integer.toString(age).toCharArray();//나이를 char[] 형태로 형변환
+        // 나이를 char[] 형태로 형변환
+        char[] ageChar = Integer.toString(age).toCharArray();
+        // 외계 행성 나이에 해당하는 문자 배열
         char[] arr = {'a','b','c','d','e','f','g','h','i','j'}; 
         
-        StringBuilder strBuilder = new StringBuilder(); //문자열로 만들기 위해 생성
+        // 문자열을 만들기 위해 StringBuilder 객체 생성
+        StringBuilder strBuilder = new StringBuilder(); 
         
+        // 나이를 외계 행성 나이 표현으로 변환
         for(char ch : ageChar){
-            int num = Character.getNumericValue(ch); //arr 배열의 인덱스
+            //int age를 char[]로 만든 각각의 원소를 인덱스로 쓰기 위해 int 형변환
+            int num = Character.getNumericValue(ch); 
+            // 외계 행성 나이에 해당하는 문자 배열 arr[num]
+            // 외계 행성 나이 표현을 StringBuilder에 추가
             strBuilder.append(arr[num]); 
-        }
-        
-       
+        }   
         return strBuilder.toString();
     }
 }
