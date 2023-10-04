1ace51bfdb0bcc476a3b2c1ea3430e2772ae9687
GoLuckyP <111550984+GoLuckyP@users.noreply.github.com>
2023-10-04 23:06:10 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.18 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -3,15 +3,14 @@ import java.util.LinkedHashSet;
 class Solution {
     public String solution(String my_string) {
         //LinkedHashSet은 순서를 보장하고 중복을 허용하지 않는다
-            LinkedHashSet<String> set = new LinkedHashSet<String>();
+        LinkedHashSet<Character> set = new LinkedHashSet<Character>();
         StringBuilder sb = new StringBuilder();
-        String[] myStrArr = my_string.split("");
         
-        for(String str : myStrArr){
+        for(char str : my_string.toCharArray()){
             //중복일 경우 false이기 때문에 if문이 실행되지 않는다
             if(set.add(str)){
-            //StringBuilder sb에 str을 하나씩 추가   
-            sb.append(str);          
+                //StringBuilder sb에 str을 하나씩 추가   
+                sb.append(str);          
             }
         }
      return sb.toString();
