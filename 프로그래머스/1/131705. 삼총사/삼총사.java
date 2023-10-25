0a74d01df8a11afe48f2de3a5c1601f3d473ac96
GoLuckyP <111550984+GoLuckyP@users.noreply.github.com>
2023-10-25 15:22:10 +0900

    [level 1] Title: 삼총사, Time: 1.63 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,28 @@
+/*삼총사 */
+/*투 포인터 기법*/
+import java.util.*;
+class Solution {
+    public int solution(int[] number) {
+
+        int answer = 0;
+        int n = number.length;
+
+        for (int i = 1; i < (1 << n); i++) {
+            int sum = 0;
+            List<Integer> selectedStudents = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) {
+                    sum += number[j];
+                    selectedStudents.add(j);
+                }
+            }
+
+            if (sum == 0 && selectedStudents.size() == 3) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
