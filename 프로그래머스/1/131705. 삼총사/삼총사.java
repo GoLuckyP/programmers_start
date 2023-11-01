717314bc4443599d0bb5f803f2d5442255fba74b
GoLuckyP <111550984+GoLuckyP@users.noreply.github.com>
2023-11-01 12:30:05 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,28 +1,41 @@
-/*삼총사 */
-/*투 포인터 기법*/
+/*삼총사 3*/
+/*비트마스크*/
+
 import java.util.*;
 class Solution {
     public int solution(int[] number) {
 
-        int answer = 0;
-        int n = number.length;
+//         int answer = 0;
+//         int n = number.length;
 
-        for (int i = 1; i < (1 << n); i++) {
-            int sum = 0;
-            List<Integer> selectedStudents = new ArrayList<>();
+//         for (int i = 1; i < (1 << n); i++) {
+//             int sum = 0;
+//             List<Integer> selectedStudents = new ArrayList<>();
 
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) > 0) {
-                    sum += number[j];
-                    selectedStudents.add(j);
+//             for (int j = 0; j < n; j++) {
+//                 if ((i & (1 << j)) > 0) {
+//                     sum += number[j];
+//                     selectedStudents.add(j);
+//                 }
+//             }
+
+//             if (sum == 0 && selectedStudents.size() == 3) {
+//                 answer++;
+//             }
+//         }
+
+//         return answer;
+         int answer = 0;
+        
+        for(int i = 0; i < number.length; i++) {
+            for(int j = i + 1; j < number.length; j++) {
+                for(int k = j + 1; k < number.length; k++) {
+                    if(number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
                 }
             }
-
-            if (sum == 0 && selectedStudents.size() == 3) {
-                answer++;
-            }
         }
-
         return answer;
     }
 }
