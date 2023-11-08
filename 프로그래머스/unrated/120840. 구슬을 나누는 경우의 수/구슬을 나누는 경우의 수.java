49ce37d328c25d2eb6fe704396cbbe7c19d4e6ec
GoLuckyP <111550984+GoLuckyP@users.noreply.github.com>
2023-11-08 21:52:49 +0900

    [level unrated] Title: 구슬을 나누는 경우의 수, Time: 1.60 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -0,0 +1,21 @@
+/*구슬을 나누는 경우의 수*/
+import java.math.BigInteger;
+class Solution {
+    public int solution(int balls, int share) {
+         // 조합 공식 nCr = n! / (n-r)!r!
+        BigInteger nFactorial  = factorial(BigInteger.valueOf(balls));
+        BigInteger rFactorial = factorial(BigInteger.valueOf(share));
+        BigInteger kFactorial = factorial(BigInteger.valueOf(balls-share));
+        
+        return nFactorial.divide(kFactorial.multiply(rFactorial)).intValue();
+    }
+
+    public static BigInteger factorial(BigInteger num){
+
+        if(num.equals(BigInteger.ZERO) || num.equals(BigInteger.ONE)){
+            return BigInteger.ONE;
+            }else{
+            return num.multiply(factorial(num.subtract(BigInteger.ONE)));
+            }
+    }
+}
